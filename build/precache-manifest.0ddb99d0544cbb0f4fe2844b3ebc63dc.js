7117bbff0f43e82b98dfc4319e54c0c11ee5bd81
Teju-cloud <theju0190@gmail.com>
2020-02-27 16:21:41 +0530

    updated

---
+++ build/precache-manifest.0ddb99d0544cbb0f4fe2844b3ebc63dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9269d19f4b114bd4d0bc4a0576e6d092",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342ef956e1352b9d7877",
+    "url": "/static/css/main.421305eb.chunk.css"
+  },
+  {
+    "revision": "d59eb4e9f6f6469d1878",
+    "url": "/static/js/2.94870823.chunk.js"
+  },
+  {
+    "revision": "342ef956e1352b9d7877",
+    "url": "/static/js/main.771bedce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
